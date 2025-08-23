d8bb47994bdde1ab4a6701426771186beeb7e590
rohitpatel-dev <rohitrampatel185@gmail.com>
2025-08-23 19:48:34 +0530

    fix

--- server.js
+++ server.js
@@ -1,1 +1,17 @@
-console.log("Hello World");
+import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
+
+const app = express();
+
+// Example route
+app.get("/", (req, res) => {
+  res.send("Hello World from Express on Render!");
+});
+
+// Render will provide the port via env variable
+const PORT = process.env.PORT || 8000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
